6247ec9689c77212e92248dd87dca60c818ee68a
rs-akane <rs.akane.y@gmail.com>
2025-04-21 13:41:21 +0900

    add _index

---
+++ app/routes/recipes._index.tsx
@@ -0,0 +1,35 @@
+import { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { IndexResponse } from "~/types/recipes/IndexResponse";
+import { RecipeItem } from "~/components/recipes/RecipeItem";
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Recipes" },
+    { name: "description", content: "recipes 一覧ページ" },
+  ];
+};
+
+export const loader = async () => {
+  const response = await fetch("https://koji-book.vercel.app/api/recipes");
+  const recipes = await response.json();
+  return new Response(JSON.stringify(recipes), {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=300",
+    },
+  });
+};
+
+export default function Recipes() {
+  const recipes: IndexResponse = useLoaderData();
+  return (
+    <>
+      <h1 className="text-4xl text-center py-10">麹帳のレシピ一覧</h1>
+      <div className="max-w-md mx-auto flex flex-col gap-5 pb-10">
+        {recipes.recipeArticles.map(recipe => (
+          <RecipeItem recipe={recipe} key={recipe.article.id} />
+        ))}
+      </div>
+    </>
+  );
+}
